ad46a82b8bb8944ae2a846caa38421ede5a46851
Lin Latt <lattln.tech@gmail.com>
2024-11-09 20:50:37 -0500

    update
    + nav bar
    + hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import BlurBackground from './components/BlurBackground'
+import NavBar from './components/NavBar'
+import Hero from './components/Hero'
+
 
 const App = () => {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    return (
+        <>
+            <BlurBackground />
+            <NavBar />
+            <main className='antialiased overflow-x-hidden max-w-7xl mx-auto relative z-10'>
+                <NavBar />
+                <Hero />
+            </main>
+        </>
+            
+
+    )
 }
 
 export default App
